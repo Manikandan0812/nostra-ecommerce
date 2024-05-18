cccdff2c1f3225753ca11c73d00ab2b2a000307e
Manikandan0812 <manialagesan2001@gmail.com>
2024-05-18 20:23:25 +0530

    Customer

--- Home/index.js
+++ Home/index.js
@@ -18,44 +18,6 @@ close.addEventListener("click",function(){
     sidebar.style.left="-70%";
 })
 //slide window
-var currentSlide=0;
 
-var slideElement=document.getElementById("slider");
-var totalWindow=slideElement.childElementCount;
-console.log(totalWindow);
 
-function next()
-{
-if(currentSlide<totalWindow)
-    {
-        currentSlide++;
-        showSlide();
-    }
-}
 
-function prev(){
-    if(currentSlide>1)
-        {
-            currentSlide--;
-            showSlide();
-        }
-}
-function showSlide()
-{
-     var slides=document.getElementById("slider").getElementsByTagName("li");
-      console.log(slides);
-     for(let index=0;index<totalWindow;index++)
-        {
-            const element =slides[index];
-            if(currentSlide==index+1)
-                {
-                    element.classList.remove("hidden");
-                    console.log("Hii")
-                }
-                else
-                {
-                    element.classList.add("hidden");
-                    console.log("Hi");
-                }
-        }
-}
