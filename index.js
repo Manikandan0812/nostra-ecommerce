cd863f381e9bc1f71980f249f0b5d907bef4c7c2
Manikandan0812 <manialagesan2001@gmail.com>
2024-05-21 21:49:44 +0530

    Js Added

--- index.js
+++ index.js
@@ -21,15 +21,65 @@ close.addEventListener("click",function(){
 
 
 //collections
-var search=document.getElementById("search__collections")
-var collection=document.querySelector(".collection__s2__items")
-var collectionList=collection.querySelectorAll(".collection__s2__items__shirts");
+
+// var collection=document.querySelector(".collection__s2__items")
+// var collectionList=collection.querySelectorAll(".collection__s2__items__shirts");
+
+// var checkbox=document.getElementsByName("checkbox");
+
+// console.log(checkbox);
+// for(i=0;i<checkbox.length;i++)
+//     {
+//         checkbox[i].addEventListener("click",function(event)
+//     {
+//          var filt =(event.target.value)
+        
+
+//     })
+//     }
 
 
-search.addEventListener("keyup",function(event){
-    var enterdValue=event.target.value.toUpperCase();
-    
-})
 
+// Get all checkboxes
+var checkboxes = document.querySelectorAll('input[type="checkbox"]');
+var collectionList = document.querySelectorAll('.collection__s2__items__shirts');
 
+// Store original display style of each item
+var originalDisplay = {};
+collectionList.forEach(function (item) {
+    originalDisplay[item] = item.style.display;
+});
 
+// Add event listener to each checkbox
+checkboxes.forEach(function (checkbox) {
+    checkbox.addEventListener("click", function () {
+        // Get the value of the clicked checkbox
+        var filterValue = this.value;
+        console.log(filterValue );
+
+        collectionList.forEach(function (item) {
+            var itemName = item.querySelector('h1').innerText;
+            console.log(itemName);
+            if (itemName.toLowerCase().includes(filterValue)) {
+                item.style.display = "block";
+            } else {
+                item.style.display = "none";
+            }
+        });
+
+        // Check if all checkboxes are unchecked
+        var allUnchecked = true;
+        checkboxes.forEach(function (cb) {
+            if (cb.checked) {
+                allUnchecked = false;
+            }
+        });
+
+        // If all checkboxes are unchecked, restore original display style
+        if (allUnchecked) {
+            collectionList.forEach(function (item) {
+                item.style.display = originalDisplay[item];
+            });
+        }
+    });
+});
